2cdb6db07b0b72086545b0eeca32fbd88fce71f8
Mehrsa <mhrs.project22@gmail.com>
2023-12-28 17:45:57 +0330

    show questions

--- js/game.js
+++ js/game.js
@@ -2,11 +2,16 @@ import formatData from "./helper.js";
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 
 const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
 let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
 
+// get data from api
 const fetchData = async () => {
     const response = await fetch(URL);
     const json = await response.json();
@@ -16,8 +21,20 @@ const fetchData = async () => {
 
 // show or unshow loader
 const start = () => {
+    showQuestion();
     loader.style.display = "none";
     container.style.display = "block";
+};
+
+// show questions
+const showQuestion = () => {
+    const { question, answers, correctAnswerIndex } = formattedData[questionIndex];
+    correctAnswer = correctAnswerIndex;
+    console.log(correctAnswer);
+    questionText.innerText = question;
+    answerList.forEach((button, index) => {
+        button.innerText = answers[index];
+    });
 };
 
 window.addEventListener("load", fetchData);
