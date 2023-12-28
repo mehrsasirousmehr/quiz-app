b063c54c786a4829a9a00521c8d338ea3e3513d7
Mehrsa <mhrs.project22@gmail.com>
2023-12-28 12:39:49 +0330

    get data from api

---
+++ js/game.js
@@ -0,0 +1,21 @@
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+
+let formattedData = null;
+
+const fetchData = async () => {
+    const response = await fetch(URL);
+    const json = await response.json();
+    formattedData = json;
+    start();
+};
+
+// show or unshow loader
+const start = () => {
+    loader.style.display = "none";
+    container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
