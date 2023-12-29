66304f13ba82e1032842d98b9c46c85dbbf8069d
Mehrsa <mhrs.project22@gmail.com>
2023-12-29 09:44:27 +0330

    save scores

---
+++ js/end.js
@@ -0,0 +1,29 @@
+const score = JSON.parse(localStorage.getItem("score"));
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const scoreELe = document.querySelector("p");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+
+// show score from localStorage to p tag
+scoreELe.innerText = score;
+
+// save the score
+const saveHandler = () => {
+    if (!input.value || !score) {
+        alert("enter something beatch");
+    } else {
+        const finalScore = {
+            name: input.value,
+            score,
+        };
+        highScores.push(finalScore);
+        highScores.sort((a, b) => b.scores - a.scores); // sort from high to low
+        highScores.splice(10); // show just 10 of high scores
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+        localStorage.removeItem("scores");
+        window.location.assign("/");
+    }
+};
+
+button.addEventListener("click", saveHandler);
